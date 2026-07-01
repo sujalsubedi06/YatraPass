c6b3fbc994cbb6489ff20bc173657cc324c906a1
sujalsubedi06 <sujal.subedi96@gmail.com>
2026-07-01 09:00:53 +0545

    refactor: rename app theme to styles for clearer architecture

--- lib/features/splash/presentation/widgets/transit_line.dart
+++ lib/features/splash/presentation/widgets/transit_line.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../../../app/theme/app_colors.dart';
+import '../../../../app/styles/app_colors.dart';
 
 class TransitLine extends StatelessWidget {
   const TransitLine({
